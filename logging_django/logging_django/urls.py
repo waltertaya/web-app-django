fd2dcb0c87c4a830a54e07a30165a14e83355ed8
waltertaya <waltermittybecker5@gmail.com>
2023-11-10 19:22:58 +0300

    static web app

--- logging_django/logging_django/urls.py
+++ logging_django/logging_django/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('blog.urls', 'blog'))),
+    path('blog/', include(('blog.urls', 'blog'))),
 ]
